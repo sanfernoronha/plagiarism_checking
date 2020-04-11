cceedf76719b70b3b0fd5f703e2a0e5dd60ae60b
sanfernoronha <sanfernoronha99@gmail.com>
2020-04-11 17:42:07 +0530

    Fixed form with validation

--- src/views/Mytopic.vue
+++ src/views/Mytopic.vue
@@ -51,48 +51,112 @@
             <v-card-title>
               <span class="headline">New Assignment</span>
             </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="12">
-                    <v-text-field v-model="subject" label="Topic Name" required></v-text-field>
-                  </v-col>
+            <v-container>
+              <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+                <v-card-text>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-text-field
+                        prepend-icon="mdi-rename-box"
+                        v-model="subject"
+                        label="Topic Name"
+                        required
+                        :rules="nameRules"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on }">
+                          <v-text-field
+                            v-model="computedDateFormatted"
+                            label="Deadline Date"
+                            hint="MM/DD/YYYY format"
+                            persistent-hint
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-on="on"
+                            :rules="dateRules"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="date" no-title @input="menu2 = false"></v-date-picker>
+                      </v-menu>
+                    </v-col>
 
-                  <v-col cols="12" sm="6" md="12">
-                    <v-file-input
-                      v-model="files"
-                      color="deep-purple accent-4"
-                      counter
-                      label="File input"
-                      placeholder="Select your files"
-                      prepend-icon="mdi-paperclip"
-                      outlined
-                      :show-size="1000"
-                    >
-                      <template v-slot:selection="{ index, text }">
-                        <v-chip
-                          v-if="index < 2"
-                          color="deep-purple accent-4"
-                          dark
-                          label
-                          small
-                        >{{ text }}</v-chip>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        :return-value.sync="time"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on }">
+                          <v-text-field
+                            v-model="time"
+                            label="Deadline Time"
+                            prepend-icon="mdi-clock"
+                            readonly
+                            v-on="on"
+                            :rules="timeRules"
+                          ></v-text-field>
+                        </template>
+                        <v-time-picker
+                          v-if="menu"
+                          v-model="time"
+                          full-width
+                          @click:minute="$refs.menu.save(time)"
+                        ></v-time-picker>
+                      </v-menu>
+                    </v-col>
 
-                        <span
-                          v-else-if="index === 2"
-                          class="overline grey--text text--darken-3 mx-2"
-                        >+{{ files.length - 2 }} File(s)</span>
-                      </template>
-                    </v-file-input>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="reset()">Close</v-btn>
-              <v-btn color="blue darken-1" text @click="add()">Save</v-btn>
-            </v-card-actions>
+                    <v-col cols="12" sm="6" md="12">
+                      <v-file-input
+                        v-model="files"
+                        color="deep-purple accent-4"
+                        counter
+                        label="File input"
+                        placeholder="Select your files"
+                        prepend-icon="mdi-paperclip"
+                        outlined
+                        :show-size="1000"
+                        :rules="fileRules"
+                      >
+                        <template v-slot:selection="{ index, text }">
+                          <v-chip
+                            v-if="index < 2"
+                            color="deep-purple accent-4"
+                            dark
+                            label
+                            small
+                          >{{ text }}</v-chip>
+
+                          <span
+                            v-else-if="index === 2"
+                            class="overline grey--text text--darken-3 mx-2"
+                          >+{{ files.length - 2 }} File(s)</span>
+                        </template>
+                      </v-file-input>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" @click="reset()">Close</v-btn>
+                  <v-btn color="blue darken-1" :disabled="!valid" @click="add()">Save</v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-container>
           </v-card>
         </v-dialog>
 
@@ -252,9 +316,7 @@
                     <v-subheader class="caption font-weight-bold">{{item.topic}}</v-subheader>
                   </v-col>
                   <v-col cols="12" sm="4">
-                    <v-subheader
-                      class="caption font-weight-bold"
-                    >{{item.date}}/{{item.month}}/{{item.year}}----{{item.hour}}:{{item.minutes}}</v-subheader>
+                    <v-subheader class="caption font-weight-bold">{{item.deadline}}</v-subheader>
                   </v-col>
                   <v-col cols="12" sm="4">
                     <v-subheader class="caption font-weight-bold">{{item.total}}</v-subheader>
@@ -280,10 +342,10 @@ export default {
   name: "Mytopic",
 
   data: () => ({
-    files: [],
+    files: null,
     dialog: false,
     subject: "",
-    date: Date.now(),
+
     url: "",
     subjects: [
       {
@@ -306,10 +368,51 @@ export default {
       }
     ],
     topics: [],
-    upload: ""
+    upload: "",
+
+    //datepickers
+    menu2: false,
+    date: "",
+
+    //timepickers
+    time: null,
+    menu: false,
+    valid: true,
+    lazy: false,
+    nameRules: [v => !!v || "Title is required"],
+    dateRules: [v => !!v || "Date is required"],
+    timeRules: [v => !!v || "Time is required"],
+    fileRules: [v => !!v || "File is required"]
   }),
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date);
+    }
+  },
+  /* eslint-disable */
+  watch: {
+    date(val) {
+      this.dateFormatted = this.formatDate(this.date);
+    }
+  },
+  /* eslint-enable */
+
   methods: {
     /* eslint-disable */
+
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${month}/${day}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    },
+
     add() {
       var file = this.files;
       var ref = firebase
@@ -333,7 +436,8 @@ export default {
             db.collection("Assignments")
               .doc(this.subject)
               .set({
-                deadline: this.date,
+                deadline: this.formatDate(this.date),
+                time: this.time,
                 subject: this.$route.params.id,
                 url: this.url,
                 total: 0
@@ -350,6 +454,7 @@ export default {
       (this.subject = ""), (this.subjectId = ""), (this.myclass = "");
       this.dialog = false;
       this.files = [];
+      this.$router.go();
     },
     remove() {
       this.subjects.pop();
@@ -369,32 +474,9 @@ export default {
         querySnapshot.forEach(doc => {
           let data = {
             topic: doc.id,
-            deadline: doc
-              .data()
-              .deadline.toDate()
-              .getDate(),
+            deadline: doc.data().deadline,
             url: doc.data().url,
-            total: doc.data().total,
-            date: doc
-              .data()
-              .deadline.toDate()
-              .getDate(),
-            month: doc
-              .data()
-              .deadline.toDate()
-              .getMonth(),
-            year: doc
-              .data()
-              .deadline.toDate()
-              .getYear(),
-            hour: doc
-              .data()
-              .deadline.toDate()
-              .getHours(),
-            minutes: doc
-              .data()
-              .deadline.toDate()
-              .getMinutes()
+            total: doc.data().total
           };
 
           this.topics.push(data);
